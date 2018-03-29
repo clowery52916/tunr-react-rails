d9500f8357635722125c199fac7e808441882ec9
clowery <clowery52916@gmail.com>
2018-03-29 16:27:56 -0400

    adding single artist info and styling

--- client/src/components/ArtistListView.js
+++ client/src/components/ArtistListView.js
@@ -1,8 +1,9 @@
-mport React, {Component} from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Card, Image} from 'semantic-ui-react'
 import styled from 'styled-components'
+
 
 const FlexCards = styled.div `
   display: flex;
@@ -19,7 +20,7 @@ class ArtistListView extends Component {
   }
   // we need state here because we are calling to our database to get the list of artist from api
 
-  componentWillMount() {
+  componentDidMount() {
     // initiate API call to rails
     this.fetchArtists();
     //when successful, update the state
@@ -28,6 +29,7 @@ class ArtistListView extends Component {
     const res = await axios.get('/api/artists')
     this.setState({artists: response.data.artists})
   }
+  //above is just async & await
   //here we are making a function to call on when we want to call on this anywhere throughout the App
   //via the axios call - we are basically bringing this into it's own method that is encapsulating it in
   //it's own little space that we can call on over and over and over again
@@ -41,18 +43,20 @@ class ArtistListView extends Component {
   //this.setState({artists: response.data.artists })
   //we are calling on all the artists NOTE: artists: can be called anything you want, but we are saying what
   //within our state we want to apply the data to, we want the response of the data for all the artists
-  fetchArtists = async () => {
-    try {
-      const res = await axios.get('/api/artists');
-      await this.setState({artists: res.data});
-      return res.data;
-    } catch (err) {
-      console.log(err)
-      await this.setState({error: err.message})
-      return err.message
-    }
 
-  }
+  // fetchArtists = async () => {
+  //   try {
+  //     const res = await axios.get('/api/artists');
+  //     await this.setState({artists: res.data});
+  //     return res.data;
+  //   } catch (err) {
+  //     console.log(err)
+  //     await this.setState({error: err.message})
+  //     return err.message
+  //   }
+  //
+  // }
+  // this is async and await with a try catch!
 
   render() {
     if (this.state.error) {
@@ -67,20 +71,21 @@ class ArtistListView extends Component {
             //the first time it renders on mount, it will return a blank screen, but then
             //once we set this state here, and use an asyncronis call to update this state
             //to render this return of all the artists
-            return (<Card key={artist.id}>
-              <Link to={`/artist/${artist.id}`}>
-                <Image src={artist.photo_url}/>
-                <Card.Content>
-                  <Card.Header>{artist.name}</Card.Header>
-                  <Card.Meta>{artist.nationality}</Card.Meta>
-                </Card.Content>
-              </Link>
-            </Card>)
-          })
-        }
+            return (
+              <Card key={artist.id}>
+                <Link to={`/artist/${artist.id}`}>
+                  <Image src={artist.photo_url}/>
+                  <Card.Content>
+                    <Card.Header>{artist.name}</Card.Header>
+                    <Card.Meta>{artist.nationality}</Card.Meta>
+                  </Card.Content>
+                </Link>
+              </Card>
+            )
+           })}
       </FlexCards>
     </div>)
   }
 }
 
-export default ArtistList;
+export default ArtistListView;
--- client/src/components/SingleArtistView.js
+++ client/src/components/SingleArtistView.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
+import {Card, Image} from 'semantic-ui-react'
+import styled from 'styled-components'
 
 class SingleArtistView extends Component {
   state = {
@@ -7,44 +10,50 @@ class SingleArtistView extends Component {
     songs: []
   }
   //creating a default state for our single artists
+
+  componentDidMount() {
+    this.getSingleArtist()
+    //we are making an api call so we need to do a componentDidMount
+    //props are just passing information down, props dont have to be state, could be info from api!
+  } {}
+
+  getSingleArtist = async () => {
+    const artistId = this.props.match.params.id
+    //here are are calling on the built in props that are supplied by the react ruter dom
+    const res = await axios.get(`/api/artist/${artistId}`)
+    //and here we are getting the response from the us calling on the props we were calling on
+    console.log(res.data)
+    this.setState(artist : res.data.artist, songs : res.data.songs
+    //here we are setting the new state here to make get all the info from the artist and all of the artist
+    //songs))
+  }
+  //   catch (error) {
+  //   console.log(error)
+  //   await this.setState({error: error.message})
+  //   this is just the error message we want to use if something doesnt work out
+  // }
+
+  render() {
+    return (<Container>
+      <Image src={this.state.artist.photo_url} alt=""/>
+      <Header>{this.state.artist.name}</Header>
+      <h4>Nationality: {this.state.artist.nationality}</h4>
+      <Divider/>
+      <List>
+        {
+          this.state.songs.map(song => {
+            return (<List.item key={song.id}>
+              {song.title}
+            </List.item>)
+          })
+        }
+      </List>
+      <h4>{song.title}</h4>
+      <audio controls="controls" src={song.preview_url}></audio>
+    </Container>)
+  }
 }
 
-componentDidMount() {
-  this.getSingleArtist()
-  //we are making an api call so we need to do a componentDidMount
-  //props are just passing information down, props dont have to be state, could be info from api!
-
-}
-
-getSingleArtist = async () => {
-  const artistId = this.props.match.params.id
-  //here are are calling on the built in props that are supplied by the react ruter dom
-  const res = await axios.get(`/api/artist/${artistId}`)
-  //and here we are getting the response from the us calling on the props we were calling on
-  console.log(res.data)
-  this.setState(
-    artist: res.data.artist,
-    songs: res.data.songsRes
-  //here we are setting the new state here to make get all the info from the artist and all of the artist
-  //songs
-)}  catch (error) {
-    console.log(error)
-    await this.setState({error: error.message})
-    //this is just the error message we want to use if something doesnt work out
-
-render() {
-  return (<div>
-    <img src={this.state.artist.photo_url} alt=""/>
-    <h1>{this.state.artist.name}</h1>
-    {
-      this.state.songs.map(song => (<div key={song.id}>
-        <h4>{song.title}</h4>
-        <audio controls="controls" src={song.preview_url}></audio>
-      </div>))
-    }
-  </div>);
-}
-//
 //app is the parent so, allArtists are children, singleArtists are childre and the NavBar is the children
 
-export default Artist;
+export default SingleArtistView;
